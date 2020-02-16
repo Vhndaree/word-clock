1fe1c6d640579a5ca7e7785baec84faa468d178d
Ram Kumar Bhandari <rambhandari@lftechnology.com>
2020-02-16 20:58:56 +0545

    Add constants for hour and minute.

---
+++ js/constant.js
@@ -0,0 +1,42 @@
+const CLOCK_ALPHABETS = [
+  'I', 'T', 'L', 'I', 'S', 'A', 'S', 'T', 'I', 'M', 'E',
+  'A', 'C', 'Q', 'U', 'A', 'R', 'T', 'E', 'R', 'D', 'C',
+  'T', 'W', 'E', 'N', 'T', 'Y', 'F', 'I', 'V', 'E', 'X',
+  'H', 'A', 'L', 'F', 'B', 'T', 'E', 'N', 'F', 'T', 'P',
+  'P', 'A', 'S', 'T', 'O', 'R', 'U', 'N', 'I', 'N', 'E',
+  'O', 'N', 'E', 'S', 'I', 'X', 'T', 'H', 'R', 'E', 'E',
+  'F', 'O', 'U', 'R', 'F', 'I', 'V', 'E', 'T', 'W', 'O',
+  'E', 'I', 'G', 'H', 'T', 'E', 'L', 'E', 'V', 'E', 'N',
+  'S', 'E', 'V', 'E', 'N', 'T', 'W', 'E', 'L', 'V', 'E',
+  'T', 'E', 'N', 'S', 'E', 'O', 'C', 'L', 'O', 'C', 'K'
+];
+
+const HOUR_TEXT = {
+  1: 'ONE',
+  2: 'TWO',
+  3: 'THREE',
+  4: 'FOUR',
+  5: 'FIVE',
+  6: 'SIX',
+  7: 'SEVEN',
+  8: 'EIGHT',
+  9: 'NINE',
+  10: 'TEN',
+  11: 'ELEVEN',
+  12: 'TWELVE'
+}
+
+const MINUTE_TEXT = {
+  0: 'O CLOCK',
+  5: 'FIVE PAST',
+  10: 'TEN PAST',
+  15: 'A QUARTER PAST',
+  20: 'TWENTY PAST',
+  25: 'TWENTY FIVE PAST',
+  30: 'HALF PAST',
+  35: 'TWENTY FIVE TO',
+  40: 'TWENTY TO',
+  45: 'A QUARTER TO',
+  50: 'TEN TO',
+  55: 'FIVE TO'
+}
